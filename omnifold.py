8629c6ca38e4313bc66a9607d11263595f8883d4
Patrick T. Komiske III <pkomiske@mit.edu>
2021-01-25 23:34:30 -0500

    removed .h5 endings

--- omnifold.py
+++ omnifold.py
@@ -1,5 +1,3 @@
-import sys
-
 import numpy as np
 
 # DCTR, reweights positive distribution to negative distribution
@@ -96,9 +94,9 @@ def omnifold(X_gen_i, Y_gen_i, X_det_i, Y_det_i, wdata, winit, det_model, mc_mod
 
         # set filepaths properly
         model_det_fp_i = model_det_fp.format(i)
-        det_model[1]['filepath'] = model_det_fp_i + '_Epoch-{epoch}.h5'
+        det_model[1]['filepath'] = model_det_fp_i + '_Epoch-{epoch}'
         model_mc_fp_i = model_mc_fp.format(i)
-        mc_model[1]['filepath'] = model_mc_fp_i + '_Epoch-{epoch}.h5'
+        mc_model[1]['filepath'] = model_mc_fp_i + '_Epoch-{epoch}'
 
         # define models
         model_det = det_model[0](**det_model[1])
@@ -108,20 +106,20 @@ def omnifold(X_gen_i, Y_gen_i, X_det_i, Y_det_i, wdata, winit, det_model, mc_mod
         if i > 0:
             print('loading models from previous iteration:')
             print(model_det_fp.format(i-1), model_mc_fp.format(i-1))
-            model_det.load_weights(model_det_fp.format(i-1) + '.h5')
-            model_mc.load_weights(model_mc_fp.format(i-1) + '.h5')
+            model_det.load_weights(model_det_fp.format(i-1))
+            model_mc.load_weights(model_mc_fp.format(i-1))
         
         # step 1: reweight sim to look like data
         w = np.concatenate((wdata, ws[-1]))
         w_train, w_val = w[perm_det[:-nval_det]], w[perm_det[-nval_det:]]
-        rw = reweight(X_det_train, Y_det_train, w_train, model_det, model_det_fp_i + '.h5',
+        rw = reweight(X_det_train, Y_det_train, w_train, model_det, model_det_fp_i,
                       fitargs, val_data=(X_det_val, Y_det_val, w_val))[invperm_det]
         ws.append(rw[len(wdata):])
 
         # step 2: reweight the prior to the learned weighting
         w = np.concatenate((ws[-1], ws[trw_ind]))
         w_train, w_val = w[perm_gen[:-nval_gen]], w[perm_gen[-nval_gen:]]
-        rw = reweight(X_gen_train, Y_gen_train, w_train, model_mc, model_mc_fp_i + '.h5',
+        rw = reweight(X_gen_train, Y_gen_train, w_train, model_mc, model_mc_fp_i,
                       fitargs, val_data=(X_gen_val, Y_gen_val, w_val))[invperm_gen]
         ws.append(rw[len(ws[-1]):])
         
@@ -130,6 +128,3 @@ def omnifold(X_gen_i, Y_gen_i, X_det_i, Y_det_i, wdata, winit, det_model, mc_mod
             np.save(weights_filename, ws)
         
     return ws
-
-if __name__ == '__main__':
-    main(sys.argv[1:])
